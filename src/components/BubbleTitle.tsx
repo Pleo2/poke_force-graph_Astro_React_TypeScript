8acaafd366b36366fe49cc668801f1f9ea292eeb
Pleo2/Jose Moreno <leooel23m@gmail.com>
2023-11-25 16:28:34 -0400

    change: delete hover over the BubbleTitle.tsx component react

--- src/components/BubbleTitle.tsx
+++ src/components/BubbleTitle.tsx
@@ -5,12 +5,11 @@ type ChildrenBubbleTitle = {
 export default function BubbleTitle({ children }: ChildrenBubbleTitle): JSX.Element {
 	return (
 		<>
-			<a
-				href={children}
-				className="bg-black text-gray-300 font-bold font-sans text-3xl w-1/2 p-2 h-max rounded-l-md rounded-tl-none justify-self-end hover:w-full hover:cursor-pointer transition-all hover:rounded-none "
+			<div
+				className="bg-black text-gray-300 font-bold font-sans text-3xl w-1/2 p-2 h-max rounded-l-md rounded-tl-none justify-self-end"
 			>
 				{children}
-			</a>
+			</div>
 		</>
 	)
 }
