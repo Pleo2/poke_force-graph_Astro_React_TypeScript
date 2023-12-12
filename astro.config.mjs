236c1a328933b050d23697ae414ba0a19134dd54
Pleo2/Jose Moreno <leooel23m@gmail.com>
2023-12-11 22:37:10 -0400

    add: adacter vercel and change the config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,19 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: 'hybrid',
+  adapter: vercel()
 });
+
+/* error   Cannot use `output: 'server'` or `output: 'hybrid'` without an adapter. Please install and configure the appropriate server adapter for your final deployment.
+ Hint:
+   See https://docs.astro.build/en/guides/server-side-rendering/ for more information.
+ Error reference:
+   https://docs.astro.build/en/reference/errors/no-adapter-installed/
+ File:
+   /vercel/path0/node_modules/.pnpm/astro@3.4.3_typescript@5.2.2/node_modules/astro/dist/core/build/static-build.js:35:11 */
